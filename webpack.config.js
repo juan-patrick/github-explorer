1ee065f564865dacf2e8cafd1138d582bed34f89
Juan Patrick <juan-p.andrade@outlook.com>
2022-05-04 06:24:48 -0300

    chore(webpack): configure webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,9 @@ module.exports = {
       template: path.resolve(__dirname, 'public', 'index.html')
     }),
   ],
+  devServer:{
+    static: path.resolve(__dirname, 'public'),
+  },
   module: {
     rules: [
       {
