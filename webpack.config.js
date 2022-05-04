b5f75a49392fa6e4eaf51cbe55fdcc0693278f02
Juan Patrick <juan-p.andrade@outlook.com>
2022-05-04 08:03:12 -0300

    chore(webpack): configure react fast refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
@@ -18,22 +18,31 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     }),
-  ],
+    isDevelopment && new ReactRefreshWebpackPlugin()
+  ].filter(Boolean),
   devServer: {
     static: path.resolve(__dirname, 'public'),
+    hot: true,
   },
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel'),
+            ].filter(Boolean)
+          }
+        }
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader', 'sass-loader']
-      },      
+      },
     ]
   }
 }
