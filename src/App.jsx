9e944dbd05df919707ccdd1b8603335dbf5f4c52
Juan Patrick <juan-p.andrade@outlook.com>
2022-05-04 08:04:08 -0300

    feat(components): create first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
-
 export function App() {
-  return <h1>Hello Test</h1>
-};
+  return <RepositoryList />
+}
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,23 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'http://www.github.com/unform/unform'
+}
+
+export function RepositoryList() {
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
+      </ul>
+    </section>
+  )
+}
